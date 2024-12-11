d8d0362a7ece7a874b29b0006151fa6cf2d8f294
PaulTreitel <paul.treitel@gmail.com>
2024-12-11 00:39:10 -0800

    so much simpler

--- 2024/src/bin/11.rs
+++ 2024/src/bin/11.rs
@@ -5,122 +5,51 @@ advent_of_code_2024::solution!(11);
 const PART_ONE_ROUNDS: i32 = 25;
 const PART_TWO_ROUNDS: i32 = 75;
 
-struct Round {
-    zero: u64,
-    one: u64,
-    two: u64,
-    three: u64,
-    four: u64,
-    five: u64,
-    six: u64,
-    seven: u64,
-    eight: u64,
-    nine: u64,
-    other: HashMap<u64, u64> // stone value -> count
-}
-
-impl Round {
-    pub fn from(stones: &Vec<u64>) -> Self {
-        let mut ct = [0; 10];
-        let mut extra = HashMap::new();
-        for &stone in stones {
-            if stone < 10 {
-                ct[stone as usize] += 1;
-            } else {
-                extra
-                    .entry(stone)
-                    .and_modify(|x| *x += 1)
-                    .or_insert(1);
-            }
-        }
-        Round {
-            zero: ct[0],
-            one: ct[1],
-            two: ct[2],
-            three: ct[3],
-            four: ct[4],
-            five: ct[5],
-            six: ct[6],
-            seven: ct[7],
-            eight: ct[8],
-            nine: ct[9],
-            other: extra,
-        }
-    }
-
-    pub fn next_round(&self) -> Self {
-        let mut ct = [0; 10];
-        let mut extra = HashMap::new();
-        ct[1] += self.zero;
-        extra.insert(2024, self.one);
-        extra.insert(4048, self.two);
-        extra.insert(6072, self.three);
-        extra.insert(8096, self.four);
-        extra.insert(10120, self.five);
-        extra.insert(12144, self.six);
-        extra.insert(14168, self.seven);
-        extra.insert(16192, self.eight);
-        extra.insert(18216, self.nine);
-        for (&stone, &stone_count) in &self.other {
-            let new_stones = get_new_stones(stone);
-            for s in new_stones {
-                if s < 10 {
-                    ct[s as usize] += stone_count;
-                } else {
-                    extra
-                    .entry(s)
-                    .and_modify(|x| *x += stone_count)
-                    .or_insert(stone_count);
-                }
-            }
-        }
-        Round {
-            zero: ct[0],
-            one: ct[1],
-            two: ct[2],
-            three: ct[3],
-            four: ct[4],
-            five: ct[5],
-            six: ct[6],
-            seven: ct[7],
-            eight: ct[8],
-            nine: ct[9],
-            other: extra,
-        }
-    }
-
-    pub fn num_rocks(&self) -> u64 {
-        let mut sum = self.zero + self.one + self.two + self.three + self.four
-            + self.five + self.six + self.seven + self.eight + self.nine;
-        sum += self.other.values().sum::<u64>();
-        sum
-    }
-}
-
 pub fn part_one(input: &str) -> Option<u64> {
-    let stones = parse_input(input);
-    let mut curr_round = Round::from(&stones);
-    for _ in 0..PART_ONE_ROUNDS {
-        curr_round = curr_round.next_round();
-    }
-    Some(curr_round.num_rocks())
+    Some(run_sim(input, PART_ONE_ROUNDS))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let stones = parse_input(input);
-    let mut curr_round = Round::from(&stones);
-    for _ in 0..PART_TWO_ROUNDS {
-        curr_round = curr_round.next_round();
-    }
-    Some(curr_round.num_rocks())
+    Some(run_sim(input, PART_TWO_ROUNDS))
 }
 
-fn parse_input(input: &str) -> Vec<u64> {
-    input
+fn parse_input(input: &str) -> HashMap<u64, u64> {
+    let mut stonemap = HashMap::new();
+    let stones: Vec<u64> = input
         .trim()
         .split(" ")
         .map(|s| s.parse::<u64>().unwrap())
-        .collect()
+        .collect();
+    for stone in stones {
+        stonemap
+            .entry(stone)
+            .and_modify(|x| *x += 1)
+            .or_insert(1);
+    }
+    stonemap
+}
+
+fn run_sim(input: &str, num_rounds: i32) -> u64 {
+    let mut stones = parse_input(input);
+    for _ in 0..num_rounds {
+        stones = next_round(&stones);
+    }
+    let count = stones.values().sum();
+    count
+}
+
+fn next_round(stones: &HashMap<u64, u64>) -> HashMap<u64, u64> {
+    let mut new = HashMap::new();
+    for (&stone, &ct) in stones {
+        let new_stones = get_new_stones(stone);
+        for s in new_stones {
+            new
+                .entry(s)
+                .and_modify(|x| *x += ct)
+                .or_insert(ct);
+        }
+    }
+    new
 }
 
 fn get_new_stones(stone: u64) -> Vec<u64> {
@@ -132,6 +61,7 @@ fn get_new_stones(stone: u64) -> Vec<u64> {
         let div_point = str_stone.len() / 2;
         let first = &str_stone[..div_point];
         let second = &str_stone[div_point..];
+
         res.push(first.parse().unwrap());
         res.push(second.parse().unwrap());
     } else {
